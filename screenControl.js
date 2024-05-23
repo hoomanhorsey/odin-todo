4c98188e18a0615fb68c0f5c05e767250f101870
Hermshy Mac <94696321+hoomanhorsey@users.noreply.github.com>
2024-05-23 15:33:23 +1000

    refactor, cleanup

--- screenControl.js
+++ screenControl.js
@@ -2,68 +2,48 @@ import { projectsObject } from "./projectsObject.js";
 import { eventListenerObject } from "./eventListeners.js";
 import { formsObject } from "./forms.js";
 
-
 const screenControlObject = {
 
     displayFullList() {
 
     const array = projectsObject.getProjectsArray();
     const categories = document.querySelector('.categories');
+
+    
     categories.innerHTML = '';
-    // add 'divs' for each category
     for (let i = 0; i < array.length; i++ ) {
         const categoryElement = document.createElement('div');
 
         categoryElement.innerHTML = array[i]['category'];
         categoryElement.classList.add('category' ); 
-        // Note, categories have spaces, and if you include them as a class you can't have spaces.
-        // So do you need to have a class based on the category name at all? And if so, can you abbreviate it? 
-        // Or use another way of referencing it?
-        const categories = document.querySelector('.categories');
+
+        // const categories = document.querySelector('.categories-container');
         categories.appendChild(categoryElement);
         // add 'p' elements for each item
         for (let j = 0; j < array[i]['items'].length; j++) {      
             let itemElement = categoryElement.appendChild(screenControlObject.setItemElementDetails(array, i, j));
             screenControlObject.itemDisplaySummary(itemElement, array, i, j);
             }}
-        // eventListenerObject.itemToggleListener();
         eventListenerObject.itemListenerExpand() 
     },
 
     setItemElementDetails(array, i, j) {
         const itemElement = document.createElement('div');
             itemElement.dataset.categoryindex = i;
-            itemElement.dataset.category = array[i]['category'];
             itemElement.dataset.itemindex = j;
-            itemElement.dataset.item = array[i]['items'][j]['title'];      
-            itemElement.classList.remove('full');
-            itemElement.classList.add('item', 'summary', `${array[i]['items'][j]['priority']}` );
+            itemElement.classList.add('item', `${array[i]['items'][j]['priority']}` );
             itemElement.id = `ref${i}${j}`;
         return itemElement;
     },
 
     itemDisplaySummary(itemElement, array, i, j) {
-
-        // console.log(array[i]['items'][j]['dueDate'])
-
-        // itemElement.classList.add('summary');
-        // itemElement.classList.remove('full');
-
-    //     itemElement.innerHTML = `<div class="itemSummary" 
-    //     data-categoryindex="${i}" 
-    //     data-itemindex = "${j}">
-    //     <p class="item-expand"> [+] </p>
-    // <p class="itemTitle" > ${array[i]['items'][j]['title']} </p> 
-    // <p> Priority: ${ array[i]['items'][j]['priority']} </p> </div>
-    // <p> Due date: ${ array[i]['items'][j]['dueDate']} </p> </div>`
-
-        itemElement.innerHTML = `<p class="item-expand"> [+] </p>
-                            <p class="itemTitle" > ${array[i]['items'][j]['title']} </p> 
-                            <p> Priority: ${ array[i]['items'][j]['priority']} </p> 
-                            <p> Due date: ${ array[i]['items'][j]['dueDate']} </p> `
+        itemElement.innerHTML = 
+            `<p class="item-expand"> [+] </p>
+            <p class="itemTitle" > ${array[i]['items'][j]['title']} </p> 
+            <p> Priority: ${ array[i]['items'][j]['priority']} </p> 
+            <p> Due date: ${ array[i]['items'][j]['dueDate']} </p> `
         eventListenerObject.itemListenerExpand();
-                        }
-        ,
+    },
 
     itemDisplayFull(itemElement, array, i, j) {
         console.log('i: ' + i + ' j: ' + j)
@@ -72,109 +52,69 @@ const screenControlObject = {
         //test for checklist
         let checklistHTML = '';
             // items in the checklist array.
-            // alert('this alert is the length of the checklist items, not sure why I did this but here it is: ' + array[i]['items'][j]['checklist'].length)
             var checkItemsNumber = (array[i]['items'][j]['checklist'].length)
 
-            const tempArray = [];
+            const tempArray = []; // TODO, prob delete don't need
 
             for (let k= 0; k < checkItemsNumber; k++) {
-            console.log('console logging each item of the array. Item: ' + k + ' ' + array[i]['items'][j]['checklist'][k]['checkItem']);
-
-            // Creates HTML based on items in checklist array - if none, then there is no HTML created.
-            // Need to change 'false' to a checkbox.
-            if (array[i]['items'][j]['checklist'][k]['checked']) {
-                checklistHTML += `<li>${array[i]['items'][j]['checklist'][k] ['checkItem']}
-                <input type="checkbox" checked></input> </li>`
-            } else {
-                checklistHTML += `<li>${array[i]['items'][j]['checklist'][k] ['checkItem']}
-                <input type="checkbox" ></input> </li>`
-            }
-
-
-            // checklistHTML += `<p>${array[i]['items'][j]['checklist'][k] ['checkItem']}
-            //  ${ array[i]['items'][j]['checklist'][0]['checked']} </p>`;
-
+            // Creates HTML based on items in checklist array - if none, then there is no HTML created.  // Need to change 'false' to a checkbox.
+                if (array[i]['items'][j]['checklist'][k]['checked']) {
+                    checklistHTML += `<li>${array[i]['items'][j]['checklist'][k] ['checkItem']}
+                                    <input type="checkbox" checked></input> </li>`
+                } else {
+                    checklistHTML += `<li>${array[i]['items'][j]['checklist'][k] ['checkItem']}
+                                    <input type="checkbox" ></input> </li>`
+                }
+            // TODO Prob delete
              // leave array here as you might need to use it to create checkboxes, but probably not as you're creating an array out of an array
             tempArray.push(array[i]['items'][j]['checklist'][k]['checkItem']);
-            // TODO
-            // generat a string to add into the bigger string below.....
             }
-            console.log('checklistHTML' + checklistHTML)
-            console.log('An array of the check list items: ' + tempArray);
+           
+    /       / Create different html based on whether there is a checklist
+            if (array[i]['items'][j]['checklist'].length !== 0) {
+                
+                itemElement.innerHTML = `
+                    <p class="item-collapse"> [-] </p>
+                    <p class="itemTitle" > ${array[i]['items'][j]['title']} </p> 
+                    <p class="item-description" > ${array[i]['items'][j]['description']} </p>
+                    <p> ${ array[i]['items'][j]['dueDate']} </p>
+
+                    <p><strong>Checklist</strong>
+                    <ul> ${ checklistHTML} </ul></p>
+
+                    <button class="editItem"> edit</button>
+                    <button class="deleteItem">delete</button> `
             
-
-    console.log(checklistHTML)
-    // TODO so you may need separate printouts based on if there is a checklist or not.....like if checklist.length === 0, then this....else then the full checklist items...
-
-        if (array[i]['items'][j]['checklist'].length !== 0) {
-        console.log('Full called, with checklist')
-        // itemElement.classList.add('full');
-        // itemElement.classList.remove('summary');
-
-        itemElement.innerHTML = `
-            <p class="item-collapse"> [-] </p>
-            <p class="itemTitle" > ${array[i]['items'][j]['title']} </p> 
-            <p class="item-description" > ${array[i]['items'][j]['description']} </p>
-            <p> ${ array[i]['items'][j]['dueDate']} </p>
-
-            <p><strong>Checklist</strong>
-            <ul> ${ checklistHTML} </ul></p>
-
-            <button class="editItem"> edit</button>
-            <button class="deleteItem">delete</button> `
-
-        } else {
-
-            console.log('Full called, no checklist')
-            itemElement.classList.add('full');
-            itemElement.classList.remove('summary');
+            } else {
     
-            itemElement.innerHTML = `
-            <p class="itemTitle" > ${array[i]['items'][j]['title']} </p> 
-            <p class="item-description" > ${array[i]['items'][j]['description']} </p>
-            <p> ${ array[i]['items'][j]['dueDate']} </p>
-    
-            <button class="editItem"> edit</button>
-            <button class="deleteItem">delete</button> </div>`
-        }
+                itemElement.innerHTML = `
+                    <p class="itemTitle" > ${array[i]['items'][j]['title']} </p> 
+                    <p class="item-description" > ${array[i]['items'][j]['description']} </p>
+                    <p> ${ array[i]['items'][j]['dueDate']} </p>
+            
+                    <button class="editItem"> edit</button>
+                    <button class="deleteItem">delete</button> </div>`
+            }
+
         eventListenerObject.itemEditListener();
         eventListenerObject.itemListenerCollapse();
         },
 
-    displayEditItemForm(e) {
-        console.log('hello, editing item')
-    
-        // Getting the item from the Project array, via the dataset index
-        console.log(e.id); 
-        console.log(e);
-        console.log(e.dataset.categoryindex + e.dataset.itemindex);
+    displayEditItemForm(e) {    
+        // Getting the item from the Project array, via the dataset indexes
     
         const categoryindex = e.dataset.categoryindex;
         const itemindex = e.dataset.itemindex;
-        console.log("get Item: " + projectsObject.getItem(e.dataset.categoryindex, e.dataset.itemindex).title);
         
         const item = projectsObject.getItem(e.dataset.categoryindex, e.dataset.itemindex);
-        console.log(item.title, item.description)
     
-        //remove event listener for Item
-        // var eventListener = switchItemDisplay(e);
-        alert('did you get this far')
-        // Display the data in a form?  
-
         // get the parent element, add a form as a new child, delete previous summary child element
-        let element = document.getElementById(e.id)
-            console.log(element)
-        
+    
             let newItemForm = document.createElement('div');
             newItemForm = formsObject.createEditForm(newItemForm, item, categoryindex, itemindex);
-            console.log(newItemForm);
              
-            element.replaceWith(newItemForm);
-
-            // element.appendChild(newItemForm);
-            // document.getElementById(e.id).remove();
+            e.replaceWith(newItemForm);
         
-        console.log(item.priority);
         let prioritySelector = document.getElementById(`priority${categoryindex+itemindex}`)
     
         for (var i = 0; i < prioritySelector.options.length; i++) {
@@ -186,7 +126,6 @@ const screenControlObject = {
 
         let statusSelector = document.getElementById(`status${categoryindex + itemindex}`)
         statusSelector.value = item.status;
-        alert(item.status);
 
 
         // - get the elment by ID
