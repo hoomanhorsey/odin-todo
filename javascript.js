67e20c4564ae09d34ea3efbb799f95e493a4392c
Hermshy Mac <94696321+hoomanhorsey@users.noreply.github.com>
2024-08-06 08:57:45 +1000

    Delete alert warning comments

--- javascript.js
+++ javascript.js
@@ -17,20 +17,15 @@ addEventListener("DOMContentLoaded", () => {
     // Check local storage session, prefilling if empty
     if (!localStorage.getItem("localArray")) {
 
-        alert('Nothing stored locally. Populating with default prefill array');
+        console.log('Nothing stored locally. Populating with default prefill array');
 
         const defaultArray = projectsObject.getDefaultProjectArray()
         localStorage.setItem("localArray", JSON.stringify(defaultArray))
         console.log(localStorage.getItem('localArray'))
+        
       } else {
-        alert('local storage already filled');
-        console.log(localStorage.getItem("localArray"))
+        console.log('local storage already filled');
       }
-// localStorage.setItem('localArray', JSON.stringify(projectsObject.getProjectArray()));
-// const localArrayJSON = localStorage.getItem('localArray');
-// const localArray = JSON.parse(localArrayJSON)
-// console.log(localArray)
-// console.log(localArray[2])
 
 screenControlObject.displayAllCategoriesAndItems()
 
